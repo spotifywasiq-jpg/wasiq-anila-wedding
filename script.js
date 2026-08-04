07acb7ce18e7f045876cefd4f8cf98dde9c3d19e
Muhammad Wasiq Naeem <spotifywasiq@gmail.com>
2026-08-04 13:58:29 -0400

    Added seconds to countdown

--- script.js
+++ script.js
@@ -40,8 +40,12 @@ const timer = setInterval(function () {
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     document.getElementById("timer").innerHTML =
-        days + " DAYS • " + hours + " HOURS • " + minutes + " MINUTES";
+    days + " DAYS • " +
+    hours + " HOURS • " +
+    minutes + " MINUTES • " +
+    seconds + " SECONDS";
 
 }, 1000);
